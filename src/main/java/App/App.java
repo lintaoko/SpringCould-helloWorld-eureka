a7ff85a85f091abcd76cc4e0b19b30828cd8785f
lintaoko <1244275215@qq.com>
2019-03-27 16:14:10 +0800

    1111

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -1,16 +1,9 @@
 package App;
 
-import com.github.pagehelper.PageHelper;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 import java.util.Properties;
 
@@ -19,20 +12,12 @@ import java.util.Properties;
 //@ComponentScan("App.App.Controller")
 //1.@EnableAutoConfiguration 自动配置 扫包范围默认当前类。
 //2.加上扫包范围
-@SpringBootApplication
 //@EnableAsync   //开启异步调用
 //@MapperScan(basePackages = {"App/Mapper"})//Mapper调用
-public class App   {
-
-
+@EnableEurekaServer
+@SpringBootApplication
+public class App{
         public static void main(String[] args) {
-            //整个程序的入口，启动springboot项目
-//        @SpringBootApplication
-            // @en..  +@co..  同级包和当前包
-            SpringApplication.run(App.class,args);
-
+            SpringApplication.run(App.class, args);
         }
-
-
-
 }
